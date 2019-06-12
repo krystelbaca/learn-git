3b21b42f08bfd729b1e3c56f59aa421bd029e406
Krystel Baca <kbaca@foundationmedicine.com>
2019-06-12 18:36:35 -0500

    t

--- t-three.js
+++ t-three.js
@@ -1,6 +1,1 @@
-/**
- * Hi Team three! Welcome
- * For this exersice please do:
- * 1. Declare a variable called: number and asign it.
- * 2. Print the number and multiplicate it by 10.
- */
+console.log('UPSSSSS')
